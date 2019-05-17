05628278c969c34576dcf15a7760cd37d51a86c7
MEPC\Divit <dsharma5095@gmail.com>
2019-05-16 20:23:22 -0400

    half contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,19 +28,22 @@ const IndexPage = () => (
                 {/* <p className="extraInfo">I'm also a big visual arts and design fanatic, and play cricket and badminton.</p> */}
                 <p className="extraInfo">I'm currently looking for internship opportunities for September 2019.</p>
                 <div className="links">
-                    <a href="https://www.github.com/divitsharma" className="github" />
-                    <a href="https://www.behance.net/divitsharma" className="behance" />
-                    <a href="https://www.github.com/divitsharma" className="resume">RESUME</a>
+                    <a href="https://www.github.com/divitsharma" target="_blank" className="github" />
+                    <a href="https://www.behance.net/divitsharma" target="_blank" className="behance" />
+                    <a href="https://www.github.com/divitsharma" target="_blank" className="resume">RESUME</a>
                 </div>
             </Layout>
 
             <div class="devBackground">
                 <Layout sectionTitle="a developer." id="developer">
                     <p>I make apps and games across Linux, Windows and Android.</p>
-                    <p>Past work experience includes Behaviour Interactive, Ford Motor Company, and UWaterloo Autonomoose. You can help by expanding this list.</p>
+                    <p>Past work experience includes <a target="_blank" href="https://www.bhvr.com">Behaviour Interactive</a>
+                        , <a target="_blank" href="https://www.ford.ca/owners/technology/sync/">Ford Motor Company</a>,
+                        and <a target="_blank" href="https://www.autonomoose.net">UWaterloo Autonomoose</a>.
+                        You can help by expanding this list.</p>
                     <div className="links">
-                        <a href="https://www.github.com/divitsharma" className="github" />
-                        <a href="https://www.github.com/divitsharma" className="resume">RESUME</a>
+                        <a href="https://www.github.com/divitsharma" target="_blank" className="github" />
+                        <a href="https://www.github.com/divitsharma" target="_blank" className="resume">RESUME</a>
                     </div>
                 </Layout>
             </div>
@@ -49,12 +52,15 @@ const IndexPage = () => (
                 <Layout sectionTitle="an artist." id="artist">
                     <p>I like to dabble in visual arts and graphic design once in a while. Check out my profile for some of my works.</p>
                     <div className="links">
-                        <a href="https://www.behance.net/divitsharma" className="resume">BEHANCE</a>
+                        <a target="_blank" href="https://www.behance.net/divitsharma" className="resume">BEHANCE</a>
                     </div>
                 </Layout>
             </div>
 
-            <Layout sectionTitle="available at">
+            <Layout sectionTitle="available at" id="contact">
+                {/* <p>Don't hesitate to reach out!</p> */}
+                <i class="material-icons">mail_outline</i><a href="mailto:dsharma5095@gmail.com">dsharma5095@gmail.com</a><br/>
+                <i class="fab fa-linkedin-in"></i><a href="https://www.linkedin.com/in/divitsharma/" target="_blank">linkedin.com/in/divitsharma</a>
             </Layout>
 
             {/* <footer>
