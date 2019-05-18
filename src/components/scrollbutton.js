04a58aca9de568ba4eff08c8e5d6acfd90f3af6e
MEPC\Divit <dsharma5095@gmail.com>
2019-05-18 14:23:07 -0400

    working on edge

--- src/components/scrollbutton.js
+++ src/components/scrollbutton.js
@@ -4,18 +4,29 @@ import scrollStyles from "./scrollbutton.module.scss"
 
 function ScrollPage(e) {
     if (window !== 'undefined') {
-        document.getElementById("main").scrollBy({
-            top: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-            behavior: 'smooth'
-        });
+        if (document.documentElement.style.getPropertyValue('--scrollButtonVisible') !== 'none') {
+            if (typeof document.getElementById("main").scrollBy !== 'undefined')
+            {
+                document.getElementById("main").scrollBy({
+                top: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+                behavior: 'smooth'
+                });
+            } else {
+                document.getElementById("main").scrollTop = document.getElementById("main").scrollTop + window.innerHeight;
+
+            }
+        } else {
+            document.getElementById("main").scrollTop = 0;
+
+        }
         // window.scrollBy(0, );
         // window.scrollBy(0, 100);
     }
 }
 
-const ScrollButton = ({className}) => {
+const ScrollButton = ({ className, id }) => {
     return (
-        <a className={className + ' ' + scrollStyles.scrollButton} onClick={ScrollPage}>
+        <a id={id} className={className + ' ' + scrollStyles.scrollButton} onClick={ScrollPage}>
             NEXT PAGE
         </a>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,23 @@ import HomeButton from "../components/homebutton"
 import "../styles/index.css"
 import "../styles/main.scss"
 
-const IndexPage = () => (
-    <main id="main">
+function onScroll() {
+    var scrollTop1 = document.getElementById("main").scrollTop;
+    var height = document.getElementById("layouts").scrollHeight - document.getElementById("contact").clientHeight;
+    if (scrollTop1 >= height - 10) {
+        // alert(scrollTop1 + " " + height);
+        document.getElementById("scrollButton").style.display = "none";
+    } else {
+        document.getElementById("scrollButton").style.display = "block";
+    }
+}
+
+const IndexPage = () => {
+    if (typeof window !== 'undefined') {
+        window.addEventListener('scroll', onScroll, true);
+    }
+
+    return (<main id="main">
         <SEO title="Home" keywords={[`divit`, `sharma`, `divit sharma`]} />
         <div className="sticky">
             <HomeButton></HomeButton>
@@ -18,32 +33,32 @@ const IndexPage = () => (
             <h1>I am</h1>
         </div>
 
-        <ScrollButton className="scrollButton">
+        <ScrollButton id="scrollButton" className="scrollButton">
 
         </ScrollButton>
 
-        <div className="layouts">
+        <div className="layouts" id="layouts">
             <Layout sectionTitle="Divit Sharma." id="top">
                 <p>I’m a software developer and designer currently studying Computer Science at the University of Waterloo.</p>
                 {/* <p className="extraInfo">I'm also a big visual arts and design fanatic, and play cricket and badminton.</p> */}
                 <p className="extraInfo">I'm currently looking for internship opportunities for September 2019.</p>
                 <div className="links">
-                    <a href="https://www.github.com/divitsharma" target="_blank" className="github" />
-                    <a href="https://www.behance.net/divitsharma" target="_blank" className="behance" />
-                    <a href="https://www.github.com/divitsharma" target="_blank" className="resume">RESUME</a>
+                    <a href="https://www.github.com/divitsharma" target="_blank" className="github" rel="noopener noreferrer" />
+                    <a href="https://www.behance.net/divitsharma" target="_blank" className="behance" rel="noopener noreferrer" />
+                    <a href="https://www.github.com/divitsharma" target="_blank" className="resume" rel="noopener noreferrer">RESUME</a>
                 </div>
             </Layout>
 
-            <div class="devBackground">
+            <div className="devBackground">
                 <Layout sectionTitle="a developer." id="developer">
                     <p>I make apps and games across Linux, Windows and Android.</p>
-                    <p>Past work experience includes <a target="_blank" href="https://www.bhvr.com">Behaviour Interactive</a>
-                        , <a target="_blank" href="https://www.ford.ca/owners/technology/sync/">Ford Motor Company</a>,
-                        and <a target="_blank" href="https://www.autonomoose.net">UWaterloo Autonomoose</a>.
+                    <p>Past work experience includes <a target="_blank" href="https://www.bhvr.com" rel="noopener noreferrer">Behaviour Interactive</a>
+                        , <a target="_blank" href="https://www.ford.ca/owners/technology/sync/" rel="noopener noreferrer">Ford Motor Company</a>,
+                        and <a target="_blank" href="https://www.autonomoose.net" rel="noopener noreferrer">UWaterloo Autonomoose</a>.
                         </p>
                     <div className="links">
-                        <a href="https://www.github.com/divitsharma" target="_blank" className="github" />
-                        <a href="https://www.github.com/divitsharma" target="_blank" className="resume">RESUME</a>
+                        <a href="https://www.github.com/divitsharma" target="_blank" className="github" rel="noopener noreferrer" />
+                        <a href="https://www.github.com/divitsharma" target="_blank" className="resume" rel="noopener noreferrer">RESUME</a>
                     </div>
                 </Layout>
             </div>
@@ -52,16 +67,16 @@ const IndexPage = () => (
                 <Layout sectionTitle="an artist." id="artist">
                     <p>I like to dabble in visual arts and graphic design once in a while. Check out my profile for some of my works.</p>
                     <div className="links">
-                        <a target="_blank" href="https://www.behance.net/divitsharma" className="resume">BEHANCE</a>
+                        <a target="_blank" href="https://www.behance.net/divitsharma" className="resume" rel="noopener noreferrer">BEHANCE</a>
                     </div>
                 </Layout>
             </div>
 
             <Layout sectionTitle="available at" id="contact">
                 {/* <p>Don't hesitate to reach out!</p> */}
-                <i class="material-icons">mail_outline</i><a href="mailto:dsharma5095@gmail.com">dsharma5095@gmail.com</a><br/>
-                <i class="fab fa-linkedin-in"></i><a href="https://www.linkedin.com/in/divitsharma/" target="_blank">linkedin.com/in/divitsharma</a><br />
-                <i class="fab fa-github"></i><a href="https://www.github.com/divitsharma" target="_blank">github.com/divitsharma</a><br/>
+                <i className="material-icons">mail_outline</i><a href="mailto:dsharma5095@gmail.com">dsharma5095@gmail.com</a><br />
+                <i className="fab fa-linkedin-in"></i><a href="https://www.linkedin.com/in/divitsharma/" target="_blank" rel="noopener noreferrer">linkedin.com/in/divitsharma</a><br />
+                <i className="fab fa-github"></i><a href="https://www.github.com/divitsharma" target="_blank" rel="noopener noreferrer">github.com/divitsharma</a><br />
             </Layout>
 
             {/* <footer>
@@ -71,6 +86,7 @@ const IndexPage = () => (
             </footer> */}
         </div>
     </main>
-)
+    )
+}
 
 export default IndexPage
